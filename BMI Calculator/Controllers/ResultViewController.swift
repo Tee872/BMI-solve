2088139d6758622c09638b968b114022e20172c6
Natee Thongsuk <nateethongsuk271@gmail.com>
2023-11-18 16:08:51 +0700

    BMI

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Natee Thongsuk on 10/28/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
+
+/*
+ bmiLabel.text = bmiValue
+ adviceLabel.text = advice
+ */
